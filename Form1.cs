785f978ea745b0e8608885a4b25822af9e8f8ba3
Mark Vincent C. Pausanos <markvincent.pausanos@cit.edu>
2022-12-08 10:10:58 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded, processed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void basicCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, pixel);
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+
+        private void greyscaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    int average = (pixel.R + pixel.G + pixel.B) / 3;
+                    processed.SetPixel(x, y, Color.FromArgb(average, average, average));
+
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+
+        private void colorInversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    processed.SetPixel(x, y, Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B));
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+        private void histogramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap loadedCopy = loaded;
+            Color pixel;
+            byte average;
+            int[] histogram = new int[256];
+
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    pixel = loaded.GetPixel(x, y);
+                    average = (byte)((pixel.R + pixel.G + pixel.B) / 3);
+                    loadedCopy.SetPixel(x, y, Color.FromArgb(average, average, average));
+
+                    pixel = loaded.GetPixel(x, y);
+                    histogram[pixel.R]++;
+                }
+            }
+
+            processed = new Bitmap(256, 800);
+
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    processed.SetPixel(x, y, Color.White);
+                }
+            }
+
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < Math.Min(histogram[x] / 5, processed.Height - 1); y++)
+                {
+                    processed.SetPixel(x, (processed.Height - 1) - y, Color.Black);
+                }
+            }
+
+            pictureBox2.Image = processed;
+        }
+
+        private void sepiaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            double tr, tg, tb;
+            for (int x = 0; x < loaded.Width; x++)
+            {
+                for (int y = 0; y < loaded.Height; y++)
+                {
+                    Color pixel = loaded.GetPixel(x, y);
+                    tr = 0.393 * pixel.R + 0.769 * pixel.G + 0.189 * pixel.B;
+                    tg = 0.349 * pixel.R + 0.686 * pixel.G + 0.168 * pixel.B;
+                    tb = 0.272 * pixel.R + 0.534 * pixel.G + 0.131 * pixel.B;
+
+                    processed.SetPixel(x, y, 
+                        Color.FromArgb(
+                            Math.Min((int) tr, 255), 
+                            Math.Min((int) tg, 255),
+                            Math.Min((int) tb, 255))
+                        );
+                }
+            }
+
+            pictureBox2.Image = processed;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+    }
+}
+    
